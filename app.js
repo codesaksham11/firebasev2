67c71e1860f228ecb8fd1f9148592d0c58883484
codesaksham11 <bhattaraij51@gmail.com>
2025-04-11 15:07:10 +0545

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,29 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "firebase/auth";
 
 // Firebase configuration (replace placeholders)
-const firebaseConfig = {
-    apiKey: "[YOUR_API_KEY]",
-    authDomain: "[YOUR_AUTH_DOMAIN]",
-    projectId: "[Your Firebase Project ID]",
-    storageBucket: "[YOUR_STORAGE_BUCKET]",
-    messagingSenderId: "[YOUR_MESSAGING_SENDER_ID]",
-    appId: "[YOUR_APP_ID]"
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCDme66sAqomkZbYxl4YMt3ACkW3zWkt0U",
+    authDomain: "mcq-first-trial.firebaseapp.com",
+    projectId: "mcq-first-trial",
+    storageBucket: "mcq-first-trial.firebasestorage.app",
+    messagingSenderId: "563813208549",
+    appId: "1:563813208549:web:0b1e61feeffdf07147df0c",
+    measurementId: "G-QCQG0CRVVD"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
 };
 
 // Initialize Firebase
